197d44807c99441d5e1c7284da0c1f038de4998f
Olatunde Oladeni <olatundeoladeni@yahoo.com>
2019-05-18 19:43:07 +0100

    Added more test suite to the TestSuitesRunner Class

---
+++ SeleniumJavaProject/src/com/JavaProjectEx/TestSuitesRunner.java
@@ -0,0 +1,16 @@
+package com.JavaProjectEx;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({DragAndDropExample.class, Facebook_Project.class, 
+	Google_Search_Functionality.class, JunitAnnotations.class, 
+	JunitProject1.class, MouseHovering.class})
+
+public class TestSuitesRunner {
+	
+	
+	
+}
